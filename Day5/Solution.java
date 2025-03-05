9c8586b45bdfb3f447c1b672c7580ae18a425318
Subhangee Sahoo <shubh190601@gmail.com>
2025-03-06 00:23:57 +0530

    String compression

---
+++ Day5/Solution.java
@@ -0,0 +1,32 @@
+package Day5;
+
+class Solution {
+    public int compress(char[] chars) {
+        int index = 0;  
+        int i = 0; 
+        
+        while (i < chars.length) {
+            char currentChar = chars[i];
+            int count = 0;
+            
+           
+            while (i < chars.length && chars[i] == currentChar) {
+                count++;
+                i++;
+            }
+            
+           
+            chars[index++] = currentChar;
+            
+           
+            if (count > 1) {
+                for (char c : Integer.toString(count).toCharArray()) {
+                    chars[index++] = c;
+                }
+            }
+        }
+        
+        return index;  
+    }
+}
+
