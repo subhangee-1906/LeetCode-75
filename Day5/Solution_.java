d4bfa039b5d324a791dafd211ddc65ac14c3f0fd
Subhangee Sahoo <shubh190601@gmail.com>
2025-03-06 23:41:23 +0530

    Move Zeroes

---
+++ Day5/Solution_.java
@@ -0,0 +1,17 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int nonZeroIndex = 0; 
+        for (int i = 0; i < nums.length; i++) 
+        {
+            if (nums[i] != 0) 
+            {
+                nums[nonZeroIndex] = nums[i]; 
+                if (i != nonZeroIndex) 
+                {
+                    nums[i] = 0; 
+                }
+                nonZeroIndex++;
+            }
+        }
+    }
+}
